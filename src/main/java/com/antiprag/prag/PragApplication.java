760fa303f70ac8bd18f2f0449a49bc30dad3596a
Thiago Garcia Santos <thigasan@gmail.com>
2026-02-05 13:36:02 -0300

    first commit

---
+++ src/main/java/com/antiprag/prag/PragApplication.java
@@ -0,0 +1,13 @@
+package com.antiprag.prag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PragApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PragApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antiprag/prag/PragApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antiprag.prag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PragApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
